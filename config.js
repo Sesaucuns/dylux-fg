3a9cb75c0b9d2053e4d3bab9d6345a48453cbcb5
Sesaucuns <127551215+Sesaucuns@users.noreply.github.com>
2024-03-13 15:48:07 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['59172945992', 'FG98', true],
-  ['5492622271736'], 
-  ['5492622271736'] 
+  ['5493735483307', 'FG98', true],
+  ['5493735569492'], 
+  ['5493735649903'] 
 ] //Numeros de owner 
 
 global.mods = ['573125484672'] 
